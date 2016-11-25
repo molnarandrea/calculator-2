411c4111710d2eadc13d31996f0be39383fbfb67
themsg <michaelperng@gmail.com>
2016-11-25 10:35:07 +0800

    Add calculator object import using module.exports and use colors package

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 var readlineSync = require('readline-sync');
+var colors = require('colors');
+var calculator = require('./calculator');
 
-var op1 = Number(readlineSync.question('Please enter a number '));
-var op2 = Number(readlineSync.question('Please enter another number '));
-var operator = readlineSync.question('Please enter an operator (+, -, *, /) ');
+var x = Number(readlineSync.question('Please enter a number '.green));
+var y = Number(readlineSync.question('Please enter another number '.green));
+var op = readlineSync.question('Please enter an operator (+, -, *, /) '.blue);
 
-var operators = {'+': function(x, y) { return x + y; },
-                 '-': function(x, y) { return x - y; },
-                 '*': function(x, y) { return x * y; },
-                 '/': function(x, y) { return x / y; }};
+var opFunctions = {'+': calculator.add,
+                   '-': calculator.subtract,
+                   '*': calculator.multiply,
+                   '/': calculator.divide };
 
-if (operators[operator] !== undefined)
-    console.log('Your answer is: ' + operators[operator](op1, op2) + '!');
+if (opFunctions[op] !== undefined)
+    console.log(colors.yellow('Your answer is: ' + opFunctions[op](x, y) + '!'));
 else
-    console.log('Invalid operator. Please try again.');
+    console.log('Invalid operator. Please try again.'.red);
